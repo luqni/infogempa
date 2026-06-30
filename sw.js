f4f6ebca7178f39c8fced546eadbc635599875b1
luqni <mluqnibaehaqi@gmail.com>
2026-06-30 07:17:42 +0700

    fix: error pwa

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'infogempa-v1';
 const urlsToCache = [
-  '/infogempa/',
-  '/infogempa/index.php',
-  '/infogempa/assets/css/styles.css',
-  '/infogempa/assets/js/scripts.js',
-  '/infogempa/assets/icon-192.png',
-  '/infogempa/assets/icon-512.png'
+  './',
+  './index.php',
+  './assets/css/styles.css',
+  './assets/js/scripts.js',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
